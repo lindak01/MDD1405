f49c35b3e1127b96fd70484d1493e6147fd60b98
Linda <lindak01@fullsail.edu>
2014-06-01 04:33:10 -0500

    add pages

---
+++ nav.php
@@ -0,0 +1,29 @@
+      <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"/>
+              <span class="icon-bar"/>
+              <span class="icon-bar"/>
+            </button>
+            <a class="navbar-brand" href="#">Linda K. Portfolio</a>
+          </div>
+          <div class="navbar-collapse collapse" style="height: 1px;">
+            <ul class="nav navbar-nav">
+              <li class="active">
+                <a href="index.html">Home</a>
+              </li>
+              <li>
+                <a href="main_pages/about.html">About</a>
+              </li>
+              <li>
+                <a href="#contact">Contact</a>
+              </li>
+              <li>
+                <a href="pages/projects.html">Projects</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
