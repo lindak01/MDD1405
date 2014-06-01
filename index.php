9fc9d04763f2986ed853d39db0719ece29bd8d37
Linda <lindak01@fullsail.edu>
2014-06-01 11:21:01 -0500

    deleted composer

--- index.php
+++ index.php
@@ -2,15 +2,6 @@
 
 require_once "models/views.php";
 
-require 'vendor/autoload.php';
-
-use OpenCloud\Rackspace;
-
-$client = new Rackspace(Rackspace::US_IDENTITY_ENDPOINT, array(
-    'username' => 'linkda01',
-    'apiKey'   => '882e9e41828b2d3f0595d0aee7ada0c2'
-));
-
 $view = new view();
 
 $view->show('header');
